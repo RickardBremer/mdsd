0ff65130d659933ef9f868d54e80dc7f4466d5f9
Alexander <A.Ask1991@gmail.com>
2015-01-03 11:19:10 +0100

    Added email code

--- HotelSystemCode/src/model/impl/emailImpl.java
+++ HotelSystemCode/src/model/impl/emailImpl.java
@@ -3,16 +3,24 @@
 package model.impl;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import model.Booking;
 import model.ModelPackage;
 import model.email;
 
 import org.eclipse.emf.common.util.EList;
-
 import org.eclipse.emf.ecore.EClass;
-
 import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+
+import com.sun.mail.smtp.SMTPTransport;
 
 /**
  * <!-- begin-user-doc -->
@@ -49,9 +57,52 @@ public class emailImpl extends MinimalEObjectImpl.Container implements email {
 	 * @generated
 	 */
 	public void send(Booking booking) {
-		// TODO: implement this method
-		// Ensure that you remove @generated or mark it @generated NOT
-		throw new UnsupportedOperationException();
+		String toAddress = booking.getCustomer().getEmail(); 
+		String fromAddress = "mddhotelbooking@gmail.com"; 
+		final String username = "mddhotelbooking@gmail.com";
+		final String password = "mercury<3";
+		final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+		final String port = "465";
+		final String host = "smtp.gmail.com";
+
+		Properties props = System.getProperties();
+		props.setProperty("mail.smtps.host", host);
+		props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
+		props.setProperty("mail.smtp.socketFactory.fallback", "false");
+		props.setProperty("mail.smtp.port", port);
+		props.setProperty("mail.smtp.socketFactory.port", port);
+		props.setProperty("mail.smtps.auth", "true");
+
+		Session session = Session.getInstance(props); 
+
+		try {
+
+			Message msg = new MimeMessage(session);
+			msg.setFrom(new InternetAddress(fromAddress));
+			msg.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(toAddress));
+
+			msg.setSubject("Hotel booking verification");
+			msg.setText("Congratulation " + booking.getCustomer().getFirstName()+" "+ booking.getCustomer().getSurname()+
+					"! You have successfully booked a stay at our hotell \n"
+					+ "From: " + booking.getFromDate() + " \n"
+							+ "to: "+ booking.getToDate() + "\n"
+							+ "Your booking ID is: " + booking.getBookingID() + 
+							"\nHave a nice stay! \n\n "
+							+ "Kind Regards, \n //The hotel staff");
+			msg.setSentDate(new Date());
+
+			SMTPTransport transport = (SMTPTransport) session.getTransport("smtps");
+			transport.connect("smtp.gmail.com", username, password);
+			transport.sendMessage(msg, msg.getAllRecipients());
+			transport.close();
+
+			System.out.println("Email sent");
+
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	/**
