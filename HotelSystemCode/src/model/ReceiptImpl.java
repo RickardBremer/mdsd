8433c885a1114d8254173d369818a22fb79d39bb
punkiq <punkiq130@gmail.com>
2015-01-06 16:57:08 +0100

    Receipt

---
+++ HotelSystemCode/src/model/ReceiptImpl.java
@@ -0,0 +1,415 @@
+/**
+ */
+package model.impl;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+
+import model.Expense;
+import model.ExpenseExpert;
+import model.ModelPackage;
+import model.Receipt;
+
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+import org.eclipse.emf.ecore.util.EObjectResolvingEList;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Receipt</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link model.impl.ReceiptImpl#getExpenses <em>Expenses</em>}</li>
+ *   <li>{@link model.impl.ReceiptImpl#getTotalCost <em>Total Cost</em>}</li>
+ *   <li>{@link model.impl.ReceiptImpl#getDate <em>Date</em>}</li>
+ *   <li>{@link model.impl.ReceiptImpl#getId <em>Id</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class ReceiptImpl extends MinimalEObjectImpl.Container implements Receipt {
+	/**
+	 * The cached value of the '{@link #getExpenses() <em>Expenses</em>}' reference list.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getExpenses()
+	 * @generated
+	 * @ordered
+	 */
+	protected EList<Expense> expenses;
+
+	/**
+	 * The default value of the '{@link #getTotalCost() <em>Total Cost</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getTotalCost()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final double TOTAL_COST_EDEFAULT = 0.0;
+
+	/**
+	 * The cached value of the '{@link #getTotalCost() <em>Total Cost</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getTotalCost()
+	 * @generated
+	 * @ordered
+	 */
+	protected double totalCost = TOTAL_COST_EDEFAULT;
+
+	/**
+	 * The default value of the '{@link #getDate() <em>Date</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getDate()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final Date DATE_EDEFAULT = null;
+
+	/**
+	 * The cached value of the '{@link #getDate() <em>Date</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getDate()
+	 * @generated
+	 * @ordered
+	 */
+	protected Date date = DATE_EDEFAULT;
+
+	/**
+	 * The default value of the '{@link #getId() <em>Id</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getId()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final int ID_EDEFAULT = 0;
+
+	/**
+	 * The cached value of the '{@link #getId() <em>Id</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getId()
+	 * @generated
+	 * @ordered
+	 */
+	protected int id = ID_EDEFAULT;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ReceiptImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return ModelPackage.Literals.RECEIPT;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EList<Expense> getExpenses() {
+		if (expenses == null) {
+			expenses = new EObjectResolvingEList<Expense>(Expense.class, this, ModelPackage.RECEIPT__EXPENSES);
+		}
+		return expenses;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public double getTotalCost() {
+		return totalCost;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setTotalCost(double newTotalCost) {
+		double oldTotalCost = totalCost;
+		totalCost = newTotalCost;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, ModelPackage.RECEIPT__TOTAL_COST, oldTotalCost, totalCost));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Date getDate() {
+		return date;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setDate(Date newDate) {
+		Date oldDate = date;
+		date = newDate;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, ModelPackage.RECEIPT__DATE, oldDate, date));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setId(int newId) {
+		int oldId = id;
+		id = newId;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, ModelPackage.RECEIPT__ID, oldId, id));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public boolean addExpense(Expense expense) {
+		// TODO: implement this method
+		// Ensure that you remove @generated or mark it @generated NOT
+		//Calculate TotalCost!!!!! ******************
+		//throw new UnsupportedOperationException();
+		//double total = 0;
+		ExpenseExpert e = new ExpenseExpertImpl();
+		boolean added = false;
+		if (expense != null){
+			expenses.add(expense);
+			
+			e.addExpense(expense);
+			totalCost =+ expense.getPrice();
+			added = true;
+		} else{
+			added = false;
+		}
+		
+	
+		
+		return added;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated NOT
+	 */
+	public boolean removeExpense(Expense expense) {
+		// TODO: implement this method
+		// Ensure that you remove @generated or mark it @generated NOT
+		//throw new UnsupportedOperationException();
+		boolean removed = false;
+		if (expense != null){
+			for (int i = 0; i < expenses.size(); i++){
+				if(expense.getId() == expenses.get(i).getId()){
+					expenses.remove(expense);
+					ExpenseExpertImpl e = new ExpenseExpertImpl();
+					e.removeExpense(expense.getId());
+				}
+			}
+			removed = true;	
+		}
+		
+		return removed;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated NOT
+	 */
+	public EList<Expense> getAllExpenses() {
+		if(expenses.isEmpty()){
+			ExpenseExpert e = new ExpenseExpertImpl();
+			expenses = e.getAllExpense(id);
+		}		
+		return expenses;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated NOT
+	 */
+	public void Receipt(int id, Date date, EList<Expense> expenses) {
+		// TODO: implement this method
+		// Ensure that you remove @generated or mark it @generated NOT
+		//throw new UnsupportedOperationException();
+		this.id = id;
+		this.date = date;
+		this.expenses = expenses;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case ModelPackage.RECEIPT__EXPENSES:
+				return getExpenses();
+			case ModelPackage.RECEIPT__TOTAL_COST:
+				return getTotalCost();
+			case ModelPackage.RECEIPT__DATE:
+				return getDate();
+			case ModelPackage.RECEIPT__ID:
+				return getId();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case ModelPackage.RECEIPT__EXPENSES:
+				getExpenses().clear();
+				getExpenses().addAll((Collection<? extends Expense>)newValue);
+				return;
+			case ModelPackage.RECEIPT__TOTAL_COST:
+				setTotalCost((Double)newValue);
+				return;
+			case ModelPackage.RECEIPT__DATE:
+				setDate((Date)newValue);
+				return;
+			case ModelPackage.RECEIPT__ID:
+				setId((Integer)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case ModelPackage.RECEIPT__EXPENSES:
+				getExpenses().clear();
+				return;
+			case ModelPackage.RECEIPT__TOTAL_COST:
+				setTotalCost(TOTAL_COST_EDEFAULT);
+				return;
+			case ModelPackage.RECEIPT__DATE:
+				setDate(DATE_EDEFAULT);
+				return;
+			case ModelPackage.RECEIPT__ID:
+				setId(ID_EDEFAULT);
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case ModelPackage.RECEIPT__EXPENSES:
+				return expenses != null && !expenses.isEmpty();
+			case ModelPackage.RECEIPT__TOTAL_COST:
+				return totalCost != TOTAL_COST_EDEFAULT;
+			case ModelPackage.RECEIPT__DATE:
+				return DATE_EDEFAULT == null ? date != null : !DATE_EDEFAULT.equals(date);
+			case ModelPackage.RECEIPT__ID:
+				return id != ID_EDEFAULT;
+		}
+		return super.eIsSet(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	@SuppressWarnings("unchecked")
+	public Object eInvoke(int operationID, EList<?> arguments) throws InvocationTargetException {
+		switch (operationID) {
+			case ModelPackage.RECEIPT___ADD_EXPENSE__EXPENSE:
+				return addExpense((Expense)arguments.get(0));
+			case ModelPackage.RECEIPT___REMOVE_EXPENSE__EXPENSE:
+				return removeExpense((Expense)arguments.get(0));
+			case ModelPackage.RECEIPT___GET_ALL_EXPENSES:
+				return getAllExpenses();
+			case ModelPackage.RECEIPT___RECEIPT__INT_DATE_ELIST:
+				Receipt((Integer)arguments.get(0), (Date)arguments.get(1), (EList<Expense>)arguments.get(2));
+				return null;
+		}
+		return super.eInvoke(operationID, arguments);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String toString() {
+		if (eIsProxy()) return super.toString();
+
+		StringBuffer result = new StringBuffer(super.toString());
+		result.append(" (totalCost: ");
+		result.append(totalCost);
+		result.append(", Date: ");
+		result.append(date);
+		result.append(", id: ");
+		result.append(id);
+		result.append(')');
+		return result.toString();
+	}
+
+} //ReceiptImpl
