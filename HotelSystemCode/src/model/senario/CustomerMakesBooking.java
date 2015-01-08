850a649e7edb65de7576088b04ed75e7b8a043f8
mangeh <sungam56@hotmail.com>
2015-01-08 12:46:53 +0100

    SAds

--- HotelSystemCode/src/model/senario/CustomerMakesBooking.java
+++ HotelSystemCode/src/model/senario/CustomerMakesBooking.java
@@ -63,18 +63,18 @@ public class CustomerMakesBooking {
 	Receipt r = mf.createReceipt();
 	customer = bookingController.createCustomer("Hulken", "Greenman", "nlarsson10@gmail.com", "Hisingen", "2100000000000000", "000", 12, 17);
 	makeBooking(customer, fDate.getTime(),tDate,"Much Peanuts", "", 1,1,bookingController,r);
-	customer = bookingController.createCustomer("Brown", "Björne", "goran @gmail.com", "Stockolm", "2100000000000000", "000", 12, 17);
+	customer = bookingController.createCustomer("Björne", "Brown", "goran@gmail.com", "Stockolm", "2100000000000002", "000", 12, 17);
 	myCalendar = new GregorianCalendar(2015, 1, 25);
 	tDate = myCalendar.getTime();
 	myCalendar = new GregorianCalendar(2015, 1, 17);
 	Date fromDate = myCalendar.getTime();
-	makeBooking(customer, fromDate,tDate,"Absolutely no peanuts", "", 2,2,bookingController,r);
-	customer = bookingController.createCustomer("Gula", "Gubben", "gula @gmail.com", "Stockolm", "2100000000000000", "000", 12, 17);
+	makeBooking(customer, fromDate,tDate,"Absolutely no peanuts", "", 2,4,bookingController,r);
+	customer = bookingController.createCustomer("Gula", "Gubben", "gula@gmail.com", "Stockolm", "2100000000000001", "000", 12, 17);
 	myCalendar = new GregorianCalendar(2015, 1, 18);
 	tDate = myCalendar.getTime();
 	myCalendar = new GregorianCalendar(2015, 1, 13);
 	fromDate = myCalendar.getTime();
-	makeBooking(customer, fromDate,tDate,"Hide some peanuts in the room", "", 5,5,bookingController,r);
+	makeBooking(customer, fromDate,tDate,"Hide some peanuts in the room", "", 2,4,bookingController,r);
 	// Search for rooms and place available rooms in a list of rooms.
 	
 	}
@@ -85,11 +85,12 @@ public class CustomerMakesBooking {
 		EList<String> roomTypes = new BasicEList<String>(); 
 		EList<Room> rooms = new BasicEList<Room>();
 		rooms = bookingController.searchRooms(fDate, tDate, guest,desiredrooms);
-		System.out.println("Rooms size " +rooms.size());
-		if(desiredrooms <= rooms.size()) {
+		if(rooms.size() > 0) {
 			for(int i = 0; i < desiredrooms; i++){
+				System.out.println("Adding one room of type " +rooms.get(0).getType() + " to booking");
 				roomTypes.add(rooms.get(0).getType());		
 			}
+			
 			// Create a booking
 			 success = bookingController.createBooking(fDate, tDate, wishes, customer, promotion, r, roomTypes);
 			// Print if the booking was a success or not.
